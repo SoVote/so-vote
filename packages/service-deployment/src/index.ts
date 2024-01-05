1f6d49aac15b5421b1938b6591bf5ef25ac39b61
Jon Pritchard <jon@node28.com>
2024-01-05 08:55:15 +0000

    Trying to fix pl stack output issue

--- packages/service-deployment/src/index.ts
+++ packages/service-deployment/src/index.ts
@@ -42,7 +42,7 @@ program.command('deploy')
       const pulumiOutputs = JSON.parse(exec('pulumi stack output --json'))
       config.outputs.forEach((output) => {
         const value = pulumiOutputs[output.githubOutputKey]
-        if (!value) throw new Error(`Missing output. Service configuration specifies there should be a pulumi output "${output.pulumiOutputKey}" but it was not found in: ${JSON.stringify(pulumiOutputs, null, 2)}`)
+        if (typeof value === 'undefined') throw new Error(`Missing output. Service configuration specifies there should be a pulumi output "${output.pulumiOutputKey}" but it was not found in: ${JSON.stringify(pulumiOutputs, null, 2)}`)
         const exportLine = `${output.githubOutputKey}=${value}`
         console.log(`Outputting: ${exportLine}`)
         exec(`echo "${exportLine}" >> ${process.env.GITHUB_OUTPUT}`)
