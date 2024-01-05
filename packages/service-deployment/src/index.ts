6580bcda6da1caf8aee9376d2d4b43628563b084
Jon Pritchard <jon@node28.com>
2024-01-05 08:52:14 +0000

    Added troubleshoot option to service deployment config

--- packages/service-deployment/src/index.ts
+++ packages/service-deployment/src/index.ts
@@ -13,9 +13,10 @@ program
 
 program.command('deploy')
   .option('--config', 'The path to the config file', 'service.config.json')
+  .option('--troubleshoot', 'Runs the script in troubleshooting mode', false)
   .action(async (options) => {
     set('-e')
-    exec('export')
+    if (options.troubleshoot) exec('export')
     let branch = process.env.GITHUB_HEAD_REF || process.env.GITHUB_REF_NAME
     if (process.env.CI !== 'true') {
       branch = exec('git rev-parse --abbrev-ref HEAD')
@@ -23,8 +24,12 @@ program.command('deploy')
     const configString = await fs.readFile(`./${options.config}`, 'utf-8')
     const config = JSON.parse(configString) as ServiceDeploymentConfig;
     console.log(`Deploying with config: ${JSON.stringify(config)}`)
-    // exec('pwd')
-    // exec('find ../code')
+    if (options.troubleshoot){
+      console.log('Where we are:')
+      exec('pwd')
+      console.log('Code directory contains:')
+      exec('find ../code')
+    }
     if (!branch) throw new Error('Current branch is not specified')
     if (branch !== 'main' && !process.env.PR_NUMBER) throw new Error('PR number is required to deploy non-prod envs')
     const stack = branch === 'main' ? `prod-${config.name}-service` : `dev-${process.env.PR_NUMBER}-${config.name}-service`
@@ -37,7 +42,7 @@ program.command('deploy')
       const pulumiOutputs = JSON.parse(exec('pulumi stack output --json'))
       config.outputs.forEach((output) => {
         const value = pulumiOutputs[output.githubOutputKey]
-        if (!value) throw new Error(`Missing output. Service configuration specifies there should be a pulumi output ${output.pulumiOutputKey} but it was not found in: ${JSON.stringify(pulumiOutputs, null, 2)}`)
+        if (!value) throw new Error(`Missing output. Service configuration specifies there should be a pulumi output "${output.pulumiOutputKey}" but it was not found in: ${JSON.stringify(pulumiOutputs, null, 2)}`)
         const exportLine = `${output.githubOutputKey}=${value}`
         console.log(`Outputting: ${exportLine}`)
         exec(`echo "${exportLine}" >> ${process.env.GITHUB_OUTPUT}`)
