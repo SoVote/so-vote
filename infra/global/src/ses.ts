2d357701ef3c11ba604540342658b6bfa0dbafec
Jon Pritchard <jon@node28.com>
2024-01-07 15:07:18 +0000

    Use SESv1

--- infra/global/src/ses.ts
+++ infra/global/src/ses.ts
@@ -1,12 +1,18 @@
-import * as aws from '@pulumi/aws';
-import * as pulumi from '@pulumi/pulumi'
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import { soVoteComZone } from "./route53";
+import { resourcePrefix } from "@rainbow-husky/www-app-infra/src/variables";
 
-const soVoteComEmailIdentity = new aws.sesv2.EmailIdentity("rainbow-husky-email-identity-sovote.com", {
-  emailIdentity: "sovote.com",
+const soVoteSesDomainIdentity = new aws.ses.DomainIdentity(`${resourcePrefix}-domain-identity`, {domain: "sovote.com"});
+const soVoteSesVerificationRecord = new aws.route53.Record(`${resourcePrefix}-verification-record`, {
+  zoneId: soVoteComZone.zoneId,
+  name: pulumi.interpolate`_amazonses.${soVoteSesDomainIdentity.id}`,
+  type: "TXT",
+  ttl: 600,
+  records: [soVoteSesDomainIdentity.verificationToken],
 });
-
-// const soVoteComEmailIdentityMailFromAttributes = new aws.sesv2.EmailIdentityMailFromAttributes("rainbow-husky-email-identity-mail-from-attributes-sovote.com", {
-//   emailIdentity: soVoteComEmailIdentity.emailIdentity,
-//   behaviorOnMxFailure: "REJECT_MESSAGE",
-//   mailFromDomain: soVoteComEmailIdentity.emailIdentity,
-// });
+const soVoteSesDomainIdentityVerification = new aws.ses.DomainIdentityVerification(`${resourcePrefix}-domain-identify-verification`, {
+  domain: soVoteSesDomainIdentity.id
+}, {
+  dependsOn: [soVoteSesVerificationRecord],
+});
