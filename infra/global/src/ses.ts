c7c2aff52f3d8ecbf436b314062ed2bb1b569fb4
Jon Pritchard <jon@node28.com>
2024-01-08 08:30:29 +0000

    Export variables in hope that will create resources

--- infra/global/src/ses.ts
+++ infra/global/src/ses.ts
@@ -3,15 +3,15 @@ import * as aws from "@pulumi/aws";
 import { soVoteComZone } from "./route53";
 import { globalResourcePrefix } from "./variables";
 
-const soVoteSesDomainIdentity = new aws.ses.DomainIdentity(`${globalResourcePrefix}-domain-identity`, {domain: "sovote.com"});
-const soVoteSesVerificationRecord = new aws.route53.Record(`${globalResourcePrefix}-verification-record`, {
+export const soVoteSesDomainIdentity = new aws.ses.DomainIdentity(`${globalResourcePrefix}-domain-identity`, {domain: "sovote.com"});
+export const soVoteSesVerificationRecord = new aws.route53.Record(`${globalResourcePrefix}-verification-record`, {
   zoneId: soVoteComZone.zoneId,
   name: pulumi.interpolate`_amazonses.${soVoteSesDomainIdentity.id}`,
   type: "TXT",
   ttl: 600,
   records: [soVoteSesDomainIdentity.verificationToken],
 });
-const soVoteSesDomainIdentityVerification = new aws.ses.DomainIdentityVerification(`${globalResourcePrefix}-domain-identify-verification`, {
+export const soVoteSesDomainIdentityVerification = new aws.ses.DomainIdentityVerification(`${globalResourcePrefix}-domain-identify-verification`, {
   domain: soVoteSesDomainIdentity.id
 }, {
   dependsOn: [soVoteSesVerificationRecord],
