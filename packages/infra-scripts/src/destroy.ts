2782381659100425e99d43a341b8295ca6950263
Jon Pritchard <jon@node28.com>
2024-01-11 07:02:35 +0000

    Update destroy script to use new service name method

--- packages/infra-scripts/src/destroy.ts
+++ packages/infra-scripts/src/destroy.ts
@@ -1,7 +1,7 @@
 import { Command } from "commander";
 import { echo, exec, set } from "shelljs";
 import fs from "fs/promises";
-import InfraConfig from "./InfraConfig";
+import { getServiceName } from "./getServiceName";
 
 export const defineDestroyScript = (program: Command) => {
   program.command('destroy')
@@ -18,9 +18,8 @@ export const defineDestroyScript = (program: Command) => {
       if(!prNumber) throw new Error('Missing required PR_NUMBER env var when running locally')
       //TODO: Find PR number by querying the GitHub API
     }
-    const configString = await fs.readFile(`./${options.config}`, 'utf-8')
-    const config = JSON.parse(configString) as InfraConfig;
-    console.log(`Destroying with config: ${JSON.stringify(config)}`)
+    const serviceName = await getServiceName()
+    console.log(`Destroying ${serviceName}`)
     if (options.troubleshoot){
       console.log('Where we are:')
       exec('pwd')
@@ -29,7 +28,7 @@ export const defineDestroyScript = (program: Command) => {
     }
     if (!branch) throw new Error('Current branch is not specified')
     if (branch !== 'main' && !prNumber) throw new Error('PR number is required to destroy non-prod envs')
-    const stack = branch === 'main' ? `prod-${config.name}-service` : `dev-${prNumber}-${config.name}-service`
+    const stack = branch === 'main' ? `prod-${serviceName}-service` : `dev-${prNumber}-${serviceName}-service`
     exec(`pulumi stack select ${stack} -c`)
     exec(`pulumi config set branch-name "${branch}"`)
     exec(`pulumi config set pr-number "${prNumber}"`)
