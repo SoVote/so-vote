56c29e3b24b558f22dc6b6a5996939b52f211958
Jon Pritchard <jon@node28.com>
2024-01-14 14:36:48 +0000

    Trying to get resources created

--- apps/web/infra/src/index.ts
+++ apps/web/infra/src/index.ts
@@ -1,9 +1,9 @@
 import * as cf from './cloudfront'
-import * as lambda from './lambda'
+export * as lambda from './lambda'
 import * as s3 from './s3'
-import * as s3BucketPolicy from './s3BucketPolicy'
-import * as dynamoDb from './dynamoDb'
-export * from './route53'
+export * as s3BucketPolicy from './s3BucketPolicy'
+export * as dynamoDb from './dynamoDb'
+export * as route53 from './route53'
 
 export const WEB_ASSETS_BUCKET = s3.webAssetsBucket.bucket
 export const WEB_CACHE_BUCKET = s3.webCacheBucket.bucket
