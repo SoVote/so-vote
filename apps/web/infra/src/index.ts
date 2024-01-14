7939f9caaf7725f0a53b01089b7c7ef954166ce2
Jon Pritchard <jon@node28.com>
2024-01-14 11:07:05 +0000

    Output bucket names

--- apps/web/infra/src/index.ts
+++ apps/web/infra/src/index.ts
@@ -5,8 +5,8 @@ import * as s3BucketPolicy from './s3BucketPolicy'
 import * as dynamoDb from './dynamoDb'
 import * as route53 from './route53'
 
-export const WEB_ASSETS_BUCKET = s3.webAssetsBucket
-export const WEB_CACHE_BUCKET = s3.webCacheBucket
-export const WEB_IMAGE_BUCKET = s3.webImageBucket
+export const WEB_ASSETS_BUCKET = s3.webAssetsBucket.bucket
+export const WEB_CACHE_BUCKET = s3.webCacheBucket.bucket
+export const WEB_IMAGE_BUCKET = s3.webImageBucket.bucket
 
 export const WEB_CLOUDFRONT_ID = cf.webDistribution.id
