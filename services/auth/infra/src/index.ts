63176a7129e8ee142579a1e6a64b30ece3844294
Jon Pritchard <jon@node28.com>
2024-02-08 08:15:26 +0000

    Standardise service outputs - a service should only ever have an api and an event consumer

--- services/auth/infra/src/index.ts
+++ services/auth/infra/src/index.ts
@@ -2,4 +2,4 @@ import { authApiLambda } from  './lambda'
 import './cloudwatch'
 import './iam'
 
-export const AUTH_SERVICE_API_FUNCTION_NAME = authApiLambda.name
+export const API_FUNCTION_NAME = authApiLambda.name
