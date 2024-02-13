ba77f65ac9f0dc29b71f539a3b9e0d67a59ae74e
Jon Pritchard <jon@node28.com>
2024-02-13 20:35:47 +0000

    Trying to get auth api lambda policy registered

--- services/auth/infra/src/index.ts
+++ services/auth/infra/src/index.ts
@@ -1,6 +1,6 @@
 import { authApiLambda } from  './lambda'
 import './cloudwatch'
-import { authApiLambdaRolePolicy } from './iam'
+import './iam'
 
 export const API_FUNCTION_NAME = authApiLambda.name
-export const API_LAMBDA_ROLE_POLICY_ID = authApiLambdaRolePolicy.id
+// export const API_LAMBDA_ROLE_POLICY_ID = authApiLambdaRolePolicy.id
