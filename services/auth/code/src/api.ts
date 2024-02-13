8024efd3a5afb7c1aa836518b9bf987a41537127
Jon Pritchard <jon@node28.com>
2024-02-13 20:53:30 +0000

    Correct expected event object

--- services/auth/code/src/api.ts
+++ services/auth/code/src/api.ts
@@ -1,8 +1,8 @@
 import { initiateLogin } from "./initiateLogin";
 
-export const handle = (event: { op: string, body: { email: string } }) => {
+export const handle = (event: { op: string, payload: { email: string } }) => {
   console.log({ event })
   switch(event.op){
-    case 'initiate-login': return initiateLogin(event.body.email)
+    case 'initiate-login': return initiateLogin(event.payload.email)
   }
 }
