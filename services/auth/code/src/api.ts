dca1c90516a20c84cd79ea885db7263c15a06ec5
Jon Pritchard <jon@node28.com>
2024-02-13 20:41:24 +0000

    Console log event object

--- services/auth/code/src/api.ts
+++ services/auth/code/src/api.ts
@@ -1,6 +1,7 @@
 import { initiateLogin } from "./initiateLogin";
 
 export const handle = (event: { op: string, body: { email: string } }) => {
+  console.log({ event })
   switch(event.op){
     case 'initiate-login': return initiateLogin(event.body.email)
   }
