ed8d375579dc55f8a0f5854b471b0961b141d178
Jon Pritchard <jon@node28.com>
2024-02-14 07:33:37 +0000

    Enable jsx

--- services/auth/code/esbuild.config.js
+++ services/auth/code/esbuild.config.js
@@ -2,6 +2,8 @@ require('esbuild').buildSync({
   entryPoints: ['src/api.ts'],
   bundle: true,
   platform: 'node',
+  jsx: 'automatic',
+  loader: { '.ts': 'tsx' },
   target: ['node18'],
   outfile: 'dist/api.js',
 })
