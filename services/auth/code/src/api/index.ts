840aac6c39464b2552f9bc1b9919a8f09cc0efa7
Jon Pritchard <jon@node28.com>
2024-02-19 22:03:21 +0000

    Await login complete

--- services/auth/code/src/api/index.ts
+++ services/auth/code/src/api/index.ts
@@ -5,6 +5,6 @@ export const handle = async (event: { op: string, payload: any }) => {
   console.log({ event })
   switch(event.op){
     case 'initiate-login': return await initiateLogin(event.payload.email, event.payload.environment);
-    case 'complete-login': return completeLogin(event.payload.token);
+    case 'complete-login': return await completeLogin(event.payload.token);
   }
 }
