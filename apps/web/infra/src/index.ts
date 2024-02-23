4eea466e2b63f7c257dd5e6d2db425cc105c4b58
Jon Pritchard <jon@node28.com>
2024-02-23 07:57:22 +0000

    output cloudfront

--- apps/web/infra/src/index.ts
+++ apps/web/infra/src/index.ts
@@ -1,12 +1,6 @@
-import * as cf from './cloudfront'
-export * as lambda from './lambda'
+export * as cf from './cloudfront'
+import * as lambda from './lambda'
 import * as s3 from './s3'
-export * as s3BucketPolicy from './s3BucketPolicy'
-export * as dynamoDb from './dynamoDb'
-export * as route53 from './route53'
-
-export const WEB_ASSETS_BUCKET = s3.webAssetsBucket.bucket
-export const WEB_CACHE_BUCKET = s3.webCacheBucket.bucket
-export const WEB_IMAGE_BUCKET = s3.webImageBucket.bucket
-
-export const WEB_CLOUDFRONT_ID = cf.webDistribution.id
+import * as s3BucketPolicy from './s3BucketPolicy'
+import * as dynamoDb from './dynamoDb'
+import * as route53 from './route53'
