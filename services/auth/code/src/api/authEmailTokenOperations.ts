5d5fa6c969bd24b562eb817e8cc345a8ebbf2921
Jon Pritchard <jon@node28.com>
2024-03-03 10:51:02 +0000

    Use auth secret from SSM parameters

--- services/auth/code/src/api/authEmailTokenOperations.ts
+++ services/auth/code/src/api/authEmailTokenOperations.ts
@@ -1,21 +1,26 @@
 import crypto from "crypto";
+import { ssm } from '@rainbow-husky/common-backend'
 
-const key = Buffer.from('8039ace2aea257276dc0fedcfd720a221f5d86603f0a951cbe2d87feca8ece8f' as string, 'hex');
+// const key = Buffer.from('8039ace2aea257276dc0fedcfd720a221f5d86603f0a951cbe2d87feca8ece8f' as string, 'hex');
 const iv = Buffer.from('fa646872cfa756d22966c1934f3a8ae3f9fdae6a', 'hex');
 const authTagLength = 16
+
+const authSecretName = `rh-shared-${process.env.BRANCH === 'main' ? 'prod' : 'dev'}-auth-secret`
 
 interface TokenBody {
   email: string,
   issued: string
 }
 
-export const generateAuthEmailToken = (email: string) => {
+export const generateAuthEmailToken = async (email: string) => {
+  const key = await ssm.getCachedSsmParameter(authSecretName)
   const cipher = crypto.createCipheriv('aes-256-gcm', key, iv, { authTagLength })
   const body: TokenBody = { email, issued: new Date().toISOString() }
   return Buffer.concat([ cipher.update(JSON.stringify(body)), cipher.final(), cipher.getAuthTag()]).toString('hex')
 }
 
-export const parseAuthEmailToken = (token: string): TokenBody => {
+export const parseAuthEmailToken = async (token: string): Promise<TokenBody> => {
+  const key = await ssm.getCachedSsmParameter(authSecretName)
   const decipher = crypto.createDecipheriv('aes-256-gcm', key, iv, { authTagLength });
   const tokenBuffer = Buffer.from(token, 'hex')
   const bodyCutoffPosition = tokenBuffer.length - authTagLength
